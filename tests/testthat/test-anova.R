7fbf27a41d734c1b5431e6ae284b14a6d835fef8
Mingrui <imingrui@umich.edu>
2022-11-12 20:45:10 -0500

    added final test for summary_lr

--- tests/testthat/test-anova.R
+++ tests/testthat/test-anova.R
@@ -4,7 +4,7 @@ test_that("anova function obtains the same output as aov function", {
   SSE_residuals = strsplit(b[7], " ")[[1]][5]
   SSE_hp = strsplit(b[7], " ")[[1]][6]
   SSE_wt = strsplit(b[7], " ")[[1]][8]
-  # run the aov
+  # run the aov function
   g <- summary(aov(mpg ~ hp + wt, mtcars))
   expect_equal(round(as.double(SSE_residuals), 4), round(g[[1]][,2][1], 4))
   expect_equal(round(as.double(SSE_hp), 4), round(g[[1]][,2][2], 4))
--- tests/testthat/test-summary_lr.R
+++ tests/testthat/test-summary_lr.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("summary_lr outputs the results in expected format", {
+  lr_ <- lr(mpg ~ hp + wt, mtcars)
+  lm_ <- lm(mpg ~ hp + wt, mtcars)
+  o_table <- summary(lm_)[[4]]
+  output <- capture.output(summary_lr(lr_))
+  est_intercept = round(o_table[,1][1], 3)
+  output_est_intercept = strsplit(output[11], " ")[[1]][3]
+  expect_equal(est_intercept[[1]], round(as.double(output_est_intercept), 3))
 })
